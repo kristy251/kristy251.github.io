0738e58e45c45a64a0d54364e697e4c1754cef83
kristy251 <kristynghitrinh@gmail.com>
2025-03-01 01:35:59 -0500

    Lab 3 update
    Added Lab 3 link

--- lab3sketch.js
+++ lab3sketch.js
@@ -1,8 +1,11 @@
 var colourPicker; // function scope
 let strokeWeightSlider;
 var bgColourPicker;
+let myRadio;
+
 
 function setup(){
+    
     createCanvas(720,300);
     colourPicker = createColorPicker('yellow');
     
@@ -14,7 +17,34 @@ function setup(){
     bgColorButton.mousePressed(repaint);
     bgColourPicker.changed( repaint ); 
     background( bgColourPicker.value() );
-}
+
+      
+        // Create a radio button element and place it
+        // in the top-left corner.
+        myRadio = createRadio();
+        myRadio.position(20, 500);
+        myRadio.size(50);
+      
+        // Add a few color options.
+        // Color values are labeled with
+        // emotions they evoke.
+        myRadio.option('red', 'love');
+        myRadio.option('yellow', 'joy');
+        myRadio.option('blue', 'trust');
+      
+        // Choose a default option.
+        myRadio.selected('yellow');
+      
+        describe('A yellow square with three options listed, "love", "joy", and "trust". The square changes color when the user selects a new option.');
+      }
+
+      
+      function drawradio() {
+        // Set the background color using the radio button.
+        let c = myRadio.value();
+        background(c);
+      }
+
 
 function draw(){
     // ellipse(mouseX, mouseY, 10,10);
